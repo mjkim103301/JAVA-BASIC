aa7fa68d5a3682e3cb570ce85f83f50a1a6871ed
mjkim103301 <mjkim103301@gmail.com>
2022-05-19 16:59:59 +0900

    feat: 타입 변환

--- src/main/java/generic/MapperSample.java
+++ src/main/java/generic/MapperSample.java
@@ -1,4 +1,24 @@
 package generic;
 
 public class MapperSample {
+    public <T> String getToString(T t) {
+        String result=t.toString();
+        System.out.println("변환된 타입: " + "String");
+        return result;
+    }
+
+    public <T> int getToInteger(T t) {
+        int result = 0;
+        if (t instanceof String) {
+            result = Integer.parseInt(t.toString());
+        } else if (t instanceof Number) {
+            result = ((Number) t).intValue();
+        } else {
+            System.out.println("변환 실패했습니다.");
+        }
+        System.out.println("변환된 타입: " + "int");
+        return result;
+    }
+
+
 }
